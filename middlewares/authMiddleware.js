64a80014236f5571162266d37a34587847095f1b
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-03-09 18:25:52 +1100

    creaated middleware for to get jwt and check if the token is valid

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,42 @@
+import { getSession } from "../model/sessionModel.js"
+import { findUserByEmail } from "../model/userModel.js"
+import { generateAccessJWT, verifyAccessJWT, verifyRefreshJWT } from "../utility/jwtHelper.js"
+import { buildErrorResponse } from "../utility/responseHelper.js"
+
+export const userAuth = async(req, res, next) => {
+  try {
+    // get accessJWT fron req headers
+    const { authorization } = req.headers
+
+    const decodedAccessJWT = verifyAccessJWT(authorization)
+
+    if(!decodedAccessJWT?.email){
+      buildErrorResponse(res, "Invalid auth token")
+      return
+    }
+
+    // if valid token
+    // check in session if the email and token combo exists
+    const session = await getSession({ userEmail: decodedAccessJWT.email, token: authorization })
+
+    if(!session?._id){
+      buildErrorResponse(res, "Invalid auth token")
+      return
+    }
+
+    // get user info
+    const user = await findUserByEmail(decodedAccessJWT.email)
+
+    if(user?._id && user?.isVerified){
+      user.password = undefined
+      req.userInfo = user
+
+      // proceed with the request
+      return next()
+    }
+
+    throw new Error("Invalid auth token");
+  } catch (error) {
+    buildErrorResponse(res, "Invalid auth token")
+  }
+}
